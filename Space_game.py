660286c055fc6651143531ccc29919da428d9a1e
MrSmith0 <90515901+MrSmith0@users.noreply.github.com>
2021-09-11 18:04:51 +0300

    Add files via upload

---
+++ Space_game.py
@@ -0,0 +1,24 @@
+import pygame, controls
+from gun import Gan
+from pygame.sprite import Group
+
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((700, 800))
+    pygame.display.set_caption("Космические защитника")
+    bg_color = (0, 0, 0)
+    gun = Gan(screen)
+    bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
+
+    while True:
+        controls.events(screen,gun, bullets)
+        gun.update_gun()        
+        controls.update(bg_color, screen, gun, inos, bullets)
+        controls.update_bullets(bullets)
+        controls.update_inos(inos)
+
+run()
